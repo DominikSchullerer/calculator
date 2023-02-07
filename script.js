75e4e0d003601ce891679123034e20dfcfdfecee
Cheranor <89839452+Cheranor@users.noreply.github.com>
2023-02-07 15:27:30 +0100

    Implement a function calling the correct operation

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ function multiyply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2
 }
+
+function operate(num1, num2, operator) {
+    let result
+    switch (operator) {
+        case '+':
+            result = add(num1, num2);
+            break;
+
+        case '-':
+            result = subtract(num1, num2);
+            break;
+
+        case '*':
+            result = multiyply(num1, num2);
+            break;
+            
+        case '/':
+            result = divide(num1, num2);
+            break;    
+    
+        default:
+            break;
+    }
+
+    return result;
+}
